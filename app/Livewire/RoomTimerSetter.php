61b07296784ffc83a61578040afdfcc369b98fae
lmafille <lmafille@imec-archives.com>
2025-08-29 13:29:42 +0200

    Fixed error when setting game duration

--- app/Livewire/RoomTimerSetter.php
+++ app/Livewire/RoomTimerSetter.php
@@ -27,13 +27,15 @@ class RoomTimerSetter extends Component
     public function updatedTimerValue($value) {
         $split = mb_split(':', $value);
 
-        $new_value = (int)$split[0] * 3600 + (int)$split[1] * 60;
-
-        if($new_value == 0) {
-            $this->room->duration_seconds = null;
-        } else {
-            $this->room->duration_seconds = $new_value;
+        if(sizeof($split) == 2) {
+            $new_value = (int)$split[0] * 3600 + (int)$split[1] * 60;
+    
+            if($new_value == 0) {
+                $this->room->duration_seconds = null;
+            } else {
+                $this->room->duration_seconds = $new_value;
+            }
+            $this->room->save();
         }
-        $this->room->save();
     }
 }
